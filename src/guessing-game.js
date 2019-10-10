9e56be4c45cb55f649c297c2edc0d5e27e67bf75
Uladzi <bivmak@gmail.com>
2019-10-10 20:47:44 +0300

    feat: implement solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.randomNum = Math.round((this.maxValue + this.minValue) / 2);
+        return this.randomNum;
     }
 
     lower() {
+        this.maxValue = this.randomNum;
 
     }
 
     greater() {
-
+        this.minValue = this.randomNum;
     }
 }
 
